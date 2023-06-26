324d77ba97b5fcc73f20c685d7879091c4a208db
Sriyazhinii <137756347+Sriyazhinii@users.noreply.github.com>
2023-06-26 12:01:11 +0530

    Create EmployeeDetdto.java

---
+++ Employee management/DTOs/EmployeeDetdto.java
@@ -0,0 +1,45 @@
+package com.employee.dto;
+
+public class EmployeeDetdto {
+
+	private String name;
+	private String mail;
+	private String address;
+	private double salary;
+	private String deptname;
+	
+	
+	
+	public String getDeptname() {
+		return deptname;
+	}
+	public void setDeptname(String deptname) {
+		this.deptname = deptname;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	
+	
+}
